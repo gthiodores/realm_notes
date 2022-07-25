7c8f1c846caaa3ef3bb7a5ddab979f3e1b236e16
Gilbert Thiodores <gthiodores71@gmail.com>
2022-07-25 15:11:40 +0800

    feat: added theming

---
+++ lib/presentation/theme/theme.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+const ColorScheme kColorScheme = ColorScheme(
+  brightness: Brightness.light,
+  primary: Colors.black,
+  onPrimary: Colors.white,
+  secondary: Colors.black54,
+  onSecondary: Colors.white,
+  error: Colors.redAccent,
+  onError: Colors.white,
+  background: Colors.white,
+  onBackground: Colors.black54,
+  surface: Colors.white,
+  onSurface: Colors.black54,
+);
+
+const AppBarTheme kAppBarTheme = AppBarTheme(
+  color: Colors.white,
+  elevation: 0,
+  actionsIconTheme: IconThemeData(color: Colors.black),
+);
