08788062653c2c0802a802be8d98031a9c0d4e62
Gilbert Thiodores <gthiodores71@gmail.com>
2022-07-25 18:24:12 +0800

    feat: added injection for repository and watch note use case

--- lib/injector.dart
+++ lib/injector.dart
@@ -1,12 +1,14 @@
 import 'package:get_it/get_it.dart';
 import 'package:realm/realm.dart';
 import 'package:realm_notes/data/realm_container.dart';
+import 'package:realm_notes/data/repository/notes_repository.dart';
 import 'package:realm_notes/domain/get_log_in_status_use_case.dart';
 import 'package:realm_notes/domain/log_in_with_email_use_case.dart';
 import 'package:realm_notes/domain/log_out_use_case.dart';
 import 'package:realm_notes/domain/register_user_use_case.dart';
 import 'package:realm_notes/domain/validate_email.dart';
 import 'package:realm_notes/domain/validate_password.dart';
+import 'package:realm_notes/domain/watch_notes_use_case.dart';
 
 final injector = GetIt.instance;
 
@@ -17,6 +19,9 @@ void injectDependencies() {
   );
   injector.registerSingleton(RealmContainer());
 
+  // Repository inject
+  injector.registerSingleton<INotesRepository>(NotesRepository(injector.get()));
+
   /// Validate credentials use case
   injector.registerFactory(() => ValidateEmail());
   injector.registerFactory(() => ValidatePassword());
@@ -26,6 +31,9 @@ void injectDependencies() {
   injector.registerFactory(() => LogInWithEmailUseCase(injector.get()));
   injector.registerFactory(() => LogOutUseCase(injector.get()));
 
-  // Register user case inject
+  // Register use case inject
   injector.registerFactory(() => RegisterUserUseCase(injector.get()));
+
+  // Note list use case
+  injector.registerFactory(() => WatchNotesUseCase(injector.get()));
 }
