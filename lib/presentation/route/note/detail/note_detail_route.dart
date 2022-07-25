df1b4abc1002a25cd1bfa60c9fcb6a8ade72139b
Gilbert Thiodores <gthiodores71@gmail.com>
2022-07-25 18:27:28 +0800

    wip: note search and detail

---
+++ lib/presentation/route/note/detail/note_detail_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NoteDetailRoute extends StatelessWidget {
+  static const String route = '/detail';
+
+  const NoteDetailRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
---
+++ lib/presentation/route/note/search/note_search_route.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class NoteSearchRoute extends StatelessWidget {
+  static const String route = '/search';
+
+  const NoteSearchRoute({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    throw UnimplementedError();
+  }
+}
