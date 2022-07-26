b9e044087aefe90b3e5df9f55f44467deb79e7f5
Gilbert Thiodores <gthiodores71@gmail.com>
2022-07-26 17:45:20 +0800

    feat: updated repository to fit the new sync rules

--- lib/data/repository/notes_repository.dart
+++ lib/data/repository/notes_repository.dart
@@ -41,17 +41,17 @@ class NotesRepository extends INotesRepository {
 
     final realm = _realmContainer.getRealmInstance();
     final query = realm.all<model.Notes>();
-    final userQuery = realm.query<model.User>('_id = "$userId"');
+    final userQuery = realm.all<model.User>();
 
-    await _realmContainer.addSubscription(query, name: 'all-notes');
-    await _realmContainer.addSubscription(userQuery, name: 'current-user');
+    await _realmContainer.addSubscription(query, name: 'notes');
+    await _realmContainer.addSubscription(userQuery, name: 'user');
 
     final note = model.Notes(
       ObjectId(),
       title,
       content,
+      userId!,
       color: color,
-      holder: userQuery.single,
     );
 
     return realm.write(() => realm.add(note));
@@ -88,14 +88,12 @@ class NotesRepository extends INotesRepository {
 
     final realm = _realmContainer.getRealmInstance();
 
-    final userQuery = realm.query<model.User>('_id = "$userId"');
+    final userQuery = realm.all<model.User>();
     final query = realm.all<model.Notes>();
 
-    await _realmContainer.addSubscription(userQuery, name: 'current-user');
-    await _realmContainer.addSubscription(query, name: 'all-notes');
+    await _realmContainer.addSubscription(userQuery, name: 'user');
+    await _realmContainer.addSubscription(query, name: 'notes');
 
-    yield* query.changes.map((event) => event.results
-        .where((element) => element.holder?.id == userId)
-        .toList());
+    yield* query.changes.map((event) => event.results.toList());
   }
 }
